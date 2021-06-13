004e46ffdbeeb68f8c0c747e2fa7c97d53a4a222
tweek <tweeky92@gmail.com>
2021-06-13 20:19:32 +0300

    lesson02

---
+++ src/model/Resume.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.Objects;
+
+/**
+ * Initial resume class
+ */
+public class Resume {
+
+    // Unique identifier
+    private String uuid;
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return uuid.equals(resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+}
